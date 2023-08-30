6b9adeedc9fe4e7014601c7219c37b7eeffaec1b
Ganesh <gbirla06@gmail.com>
2023-08-30 17:20:07 +0530

    Added Previous & next button to populate news Items/page

--- src/components/News.js
+++ src/components/News.js
@@ -15,45 +15,77 @@ export default class News extends Component {
             "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
             "publishedAt": "2020-04-27T11:41:47Z",
             "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
         }
     ]
   constructor(){
     super();
     console.log("Constructor from news component")
     this.state={
-      articles : this.articles,
-      loading : false
+      articles : [],
+      loading : false,
+      page:1
     }
   }
+  async componentDidMount(){
+    // console.log("cdm");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d4f6bf82687341569600a26311fabd77&page=1&pageSize=10`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
+    console.log(parsedData);
+  }
+
+  handleNextClick = async () =>{
+    console.log("Next");
+    if(this.state.page+1> Math.ceil(this.state.totalResults/10)){
+
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d4f6bf82687341569600a26311fabd77&page=${this.state.page+1}&pageSize=10`
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page:this.state.page+1,
+        articles:parsedData.articles
+      })
+    }
+  }
+  handlePrevClick = async () =>{
+    console.log("Prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d4f6bf82687341569600a26311fabd77&page=${this.state.page-1}&pageSize=10`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page:this.state.page-1,
+      articles:parsedData.articles
+    })
+  }
+  cnt=1;
   render() {
+    // console.log("render");
+
     return (
       <div className='container my-4'>
         <h2>NewsMonkey Top HeadLines</h2>
+        <div className="d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
         <div className="row">
           {this.state.articles.map((element)=>{
-             return <div className="col-md-4" key={element.url}>
+             return <div className="col-md-4" key={element.url?element.url:this.cnt++}>
                   <NewsItems 
-                    title={element.title.slice(0,43)} 
-                    description={element.description.slice(0,88)}  
-                    imageUrl={element.urlToImage} 
+                    title={element.title?element.title.slice(0,43):''} 
+                    description={element.description? element.description.slice(0,88):''} 
+                    imageUrl={element.urlToImage?element.urlToImage:"https://techcrunch.com/wp-content/uploads/2018/07/Apollo-ACE-thruster-firing-June.jpg?w=1154"} 
                     newsUrl={element.url} />
               </div>
           })}
         </div>
-
+        <div className="d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -9,9 +9,9 @@ export class NewsItems extends Component {
             <div className="card" style={{width: "18rem"}}>
                 <img src={imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary" style={{cursor:"pointer"}}>Read More..</a>
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" style={{cursor:"pointer"}}>Read More..</a>
                 </div>
             </div>
         </div>
