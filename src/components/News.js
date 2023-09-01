5cf5f4ec0668c027c73d7776a6bfe02821112724
Ganesh <gbirla06@gmail.com>
2023-09-01 19:51:20 +0530

    Reacr-route-dom added & news added by category wise in navbar & handlePrev + handleNext => undateNews & some extra badge added to news cart & spinner also added

--- src/components/News.js
+++ src/components/News.js
@@ -1,64 +1,61 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
+import { Spinner } from './Spinner'
+import PropTypes from 'prop-types'
 
 export default class News extends Component {
-  articles=[
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        }
-    ]
-  constructor(){
+  static defaultProps = {
+    country:'in',
+    pageSize:8,
+    category:'general'
+  }
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+  capitalizeFirstLetter = (str) =>{
+    return str[0].toUpperCase() + str.slice(1)
+  }
+  constructor(props){
     super();
-    console.log("Constructor from news component")
+    // console.log("Constructor from news component")
     this.state={
       articles : [],
       loading : false,
       page:1
     }
-  }
-  async componentDidMount(){
-    // console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d4f6bf82687341569600a26311fabd77&page=1&pageSize=10`
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
-    console.log(parsedData);
+    console.log(props.category)
+    document.title = `${this.capitalizeFirstLetter(props.category)} - NewsMonkey`
   }
 
-  handleNextClick = async () =>{
-    console.log("Next");
-    if(this.state.page+1> Math.ceil(this.state.totalResults/10)){
-
-    }
-    else{
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d4f6bf82687341569600a26311fabd77&page=${this.state.page+1}&pageSize=10`
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page:this.state.page+1,
-        articles:parsedData.articles
-      })
-    }
-  }
-  handlePrevClick = async () =>{
-    console.log("Prev");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d4f6bf82687341569600a26311fabd77&page=${this.state.page-1}&pageSize=10`
+  async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4f6bf82687341569600a26311fabd77&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
-      page:this.state.page-1,
-      articles:parsedData.articles
+      articles:parsedData.articles,
+      totalResults:parsedData.totalResults,
+      loading:false
     })
+  }
+
+  async componentDidMount(){
+    this.updateNews();
+  }
+
+  handleNextClick = async () =>{
+    this.setState({
+      page:this.state.page+1
+    })
+    this.updateNews();
+  }
+  handlePrevClick = async () =>{
+    this.setState({
+      page:this.state.page-1
+    })
+    this.updateNews();
   }
   cnt=1;
   render() {
@@ -66,25 +63,26 @@ export default class News extends Component {
 
     return (
       <div className='container my-4'>
-        <h2>NewsMonkey Top HeadLines</h2>
-        <div className="d-flex justify-content-between">
-          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
+        <h1 className="text-center" style = {{margin:'40px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} HeadLines</h1>
+        {this.state.loading && <Spinner/>}
+        
         <div className="row">
-          {this.state.articles.map((element)=>{
+          {!this.state.loading && this.state.articles.map((element)=>{
              return <div className="col-md-4" key={element.url?element.url:this.cnt++}>
                   <NewsItems 
                     title={element.title?element.title.slice(0,43):''} 
                     description={element.description? element.description.slice(0,88):''} 
                     imageUrl={element.urlToImage?element.urlToImage:"https://techcrunch.com/wp-content/uploads/2018/07/Apollo-ACE-thruster-firing-June.jpg?w=1154"} 
-                    newsUrl={element.url} />
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name} />
               </div>
           })}
         </div>
         <div className="d-flex justify-content-between">
           <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react'
 export class NewsItems extends Component {
     
     render() {
-        let {title,description,imageUrl,newsUrl} = this.props;
+        let {title,description,imageUrl,newsUrl,author,date,source} = this.props;
         return (
         <div className='my-3'>
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card">
+                {/* {console.log({source})} */}
+                <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:"1",left:"93%"} }>{source}</span>
                 <img src={imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
+                    <p class="card-text"><small className="text-danger">By {author?author:'Unknown'} on {new Date(date).toGMTString()}</small></p>
                     <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" style={{cursor:"pointer"}}>Read More..</a>
                 </div>
             </div>
