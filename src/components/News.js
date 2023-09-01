4ace2413ea1d4fb009177c87e51fba084d75b3ad
Ganesh <gbirla06@gmail.com>
2023-09-01 22:17:04 +0530

    Next & prev button removed and infinite scroll bar added

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 import { Spinner } from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
 
 export default class News extends Component {
   static defaultProps = {
@@ -22,8 +24,9 @@ export default class News extends Component {
     // console.log("Constructor from news component")
     this.state={
       articles : [],
-      loading : false,
-      page:1
+      loading : true,
+      page:1,
+      totalResults:0
     }
     console.log(props.category)
     document.title = `${this.capitalizeFirstLetter(props.category)} - NewsMonkey`
@@ -57,34 +60,53 @@ export default class News extends Component {
     })
     this.updateNews();
   }
+  fetchMoreData = async () => {
+    this.setState({
+      page:this.state.page+1
+    })
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4f6bf82687341569600a26311fabd77&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({loading:true});
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles:this.state.articles.concat(parsedData.articles),
+      totalResults:parsedData.totalResults,
+      loading:false
+    })
+
+  };
   cnt=1;
   render() {
     // console.log("render");
 
     return (
-      <div className='container my-4'>
-        <h1 className="text-center" style = {{margin:'40px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} HeadLines</h1>
-        {this.state.loading && <Spinner/>}
-        
-        <div className="row">
-          {!this.state.loading && this.state.articles.map((element)=>{
-             return <div className="col-md-4" key={element.url?element.url:this.cnt++}>
-                  <NewsItems 
-                    title={element.title?element.title.slice(0,43):''} 
-                    description={element.description? element.description.slice(0,88):''} 
-                    imageUrl={element.urlToImage?element.urlToImage:"https://techcrunch.com/wp-content/uploads/2018/07/Apollo-ACE-thruster-firing-June.jpg?w=1154"} 
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name} />
-              </div>
-          })}
-        </div>
-        <div className="d-flex justify-content-between">
-          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
-      </div>
+      <>
+        <h1 className="text-center" style = {{margin:'40px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} HeadLines</h1>        
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={<Spinner/>}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element)=>{
+                return <div className="col-md-4" key={element.url?element.url:this.cnt++}>
+                      <NewsItems 
+                        title={element.title?element.title.slice(0,43):''} 
+                        description={element.description? element.description.slice(0,88):''} 
+                        imageUrl={element.urlToImage?element.urlToImage:"https://techcrunch.com/wp-content/uploads/2018/07/Apollo-ACE-thruster-firing-June.jpg?w=1154"} 
+                        newsUrl={element.url}
+                        author={element.author}
+                        date={element.publishedAt}
+                        source={element.source.name} />
+                  </div>
+              })}
+
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
     )
   }
 }
